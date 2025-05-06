c7ad84a2fd2f55d2697aa96f16b246ea166e9c6c
MaxSebastianF <danielmaximofg@gmail.com>
2025-05-06 12:15:11 -0400

    Add random RGB background and opacity increase on mouseover per box

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,20 @@ function createGrid(size) {
     box.classList.add("box");
     box.style.width = `${sizeBox}px`;
     box.style.height = `${sizeBox}px`;
+
+    
+    box.style.opacity = "0%";
+    
+
     box.addEventListener("mouseover", () => {
-      box.style.backgroundColor = "black";
+      let currentOpacity = Number(box.style.opacity);
+      if (currentOpacity < 1) {
+        box.style.opacity = currentOpacity + 0.1;
+      }
+      box.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+
+
+
     });
 
     container.appendChild(box);
@@ -23,10 +35,10 @@ function createGrid(size) {
 createGrid(16);
 
 resizeBtn.addEventListener("click", () => {
-  let newsize = prompt("Introduce the number of squares per side (max 100)");
+  let newsize = prompt("Enter the number of squares per side (max 100)");
   newsize = parseInt(newsize);
   if (isNaN(newsize) || newsize < 1 || newsize > 100) {
-    alert("error, introduce a number between 1 and 100");
+    alert("error, Enter a number between 1 and 100");
   } else {
     createGrid(newsize);
   }
